bba2a9477c9411852ca50019ec9faa9f2c5897ae
Alex <alexandriaduell@gmail.com>
2023-01-04 15:39:29 -0500

    Completed Seeds

--- api/project/router.js
+++ api/project/router.js
@@ -3,5 +3,22 @@ const Projects = require(`./model`)
 
 const router = express.Router
 
+router.get(`/`, (req, res) => {
+    Projects.findAll()
+        .then(project => {
+            res.json(project)
+        })
+        .catch(err => console.log(`ERROR occurred in projects router, get all response`, err))
+})
+
+router.post(`/`, (req, res) => {
+    const project = req.body
+
+    Projects.add(project)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(err => console.log(`ERROR occurred in projects router, post`, err))
+})
 
 module.exports = router
---
+++ data/seeds/04_project_resources.js
@@ -0,0 +1,9 @@
+exports.seed = async function(knex) {
+  await knex('project_resources').insert([
+    {id: 1, project_id: 1, resource_id: 2},
+    {id: 2, project_id: 1, resource_id: 3},
+    {id: 3, project_id: 1, resource_id: 4}, 
+    {id: 4, project_id: 2, resource_id: 1}, 
+    {id: 5, project_id: 2, resource_id: 4}, 
+  ]);
+};
